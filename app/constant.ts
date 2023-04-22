79c7beb432de3a32db59500e4119f3c556b9da3e
cx303 <98731303+cx303@users.noreply.github.com>
2023-04-23 03:52:38 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
 export const OWNER = "tag";
 export const REPO = "1052";
-export const REPO_URL = `https://cx202.cn/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://cx202.cn/${OWNER}/${REPO}/issues`;
+export const REPO_URL = `https://www.cx202.cn/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://www.cx202.cn/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `胖脸子GTP最新`;
-export const FETCH_COMMIT_URL = `https://cx202.cn/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://cx202.cn/${OWNER}/${REPO}/tags?per_page=1`;
+export const FETCH_COMMIT_URL = `https://www.cx202.cn/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://www.cx202.cn/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
