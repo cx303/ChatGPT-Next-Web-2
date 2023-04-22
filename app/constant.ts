e8c3c4308d7558a5cd5809199c1d3a9101131e06
cx303 <98731303+cx303@users.noreply.github.com>
2023-04-23 03:35:02 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,10 +1,10 @@
-export const OWNER = "cx303";
-export const REPO = "ChatGPT-Next-Web-1";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
+export const OWNER = "tag";
+export const REPO = "1052";
+export const REPO_URL = `https://cx202.cn/${OWNER}/${REPO}`;
+export const ISSUE_URL = `https://cx202.cn/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `胖脸子GTP最新`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const FETCH_COMMIT_URL = `https://cx202.cn/${OWNER}/${REPO}/commits?per_page=1`;
+export const FETCH_TAG_URL = `https://cx202.cn/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
 export enum Path {
